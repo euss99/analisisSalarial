e5765b69f00b36d849a702b069e060140e1fa641
euss99 <euss99@hotmail.com>
2022-10-21 15:00:51 -0500

    Creando la explicación del proyecto, tablas y estilos CSS

---
+++ main.js
@@ -0,0 +1,31 @@
+/* 
+<div class="table-section">
+<table>
+    <caption>Nombre del trabajador: Juanita
+    </caption>
+    <thead>
+        <tr>
+            <th>Año</th><th>Empresa</th><th>Salario</th>
+        </tr>
+    </thead>
+    <tr>
+        <td>2018</td><td>Freelance</td><td>$250.00</td>
+    </tr>
+    <tr>
+        <td>2019</td><td>Freelance</td><td>$250.00</td>
+    </tr>
+    <tr>
+        <td>2020</td><td>Industrias Mokepon</td><td>$850.00</td>
+    </tr>
+    <tr>
+        <td>2021</td><td>Industrias Mokepon</td><td>$1050.00</td>
+    </tr>
+    <tr>
+        <td>2022</td><td>Industrias Mokepon</td><td>$1250.00</td>
+    </tr>
+    <tr>
+        <td>2023</td><td>Industrias Mokepon</td><td>$1250.00</td>
+    </tr>
+</table>
+</div> 
+*/
